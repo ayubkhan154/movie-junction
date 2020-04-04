c7459dd8e1051ddc343d43ecd4c2a15fc2b22b87
Akshit Tripathi <tripathisoft@gmail.com>
2020-04-04 14:57:48 -0400

    Added the API request and buggy data lsit

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,54 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <p>HEREEE</p>
-  </div>
+    <div id="app">
+        <p>Start your work</p>
+        <Search v-on:updateSearchText="getData" v-bind:listItems="listItems"></Search>
+        <p>{{listItems}}</p>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import Search from './components/Search.vue'
+    import axios from 'axios'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'App',
+        components: {
+            Search
+        },
+        props: {
+            searchText: String
+        },
+        data: () => {
+            return {
+                apiKey: 'e335d66d',
+                responseData: Object,
+                listItems: Array
+            }
+        },
+        methods: {
+            getData: function (searchText) {
+                if (searchText.length > 4) {
+                    console.log(searchText);
+                    axios.get(`http://www.omdbapi.com/?apikey=${this.apiKey}&s=${searchText}`)
+                        .then(function (response) {
+                            this.responseData = response
+                        }.bind(this))
+                        .then(function () {
+                            this.listItems = this.responseData.data.Search.map(x => x["Title"]);
+                        }.bind(this));
+                }
+            }
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
---
+++ src/components/Search.vue
@@ -0,0 +1,36 @@
+<template>
+  <form>
+    <input type="search" v-model="item" list="listData" name="searchBox" id="searchBox" v-on:input="$emit('updateSearchText', $event.target.value)" placeholder="Type a Movie">
+    <datalist id="listData">
+      <option v-for="item in listItems" :key="item" v-bind:value="item"/>
+    </datalist>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'Search',
+  props: {
+    msg: String,
+    listItems: Array
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
