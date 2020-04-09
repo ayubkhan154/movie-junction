376d7c3719912b5daa02d7479df138513a7c0f93
Akshit Tripathi <tripathisoft@gmail.com>
2020-04-09 17:28:59 -0400

    Add MovieCarousel to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,35 @@
 <template>
     <div id="app">
-        <h1>Movie Junction</h1>
-        <Search />
+        <img alt="Logo" src="logo2.png">
+        <Search @get-posters="getPosters"/>
+        <MovieCarousel :posters="posterData"/>
     </div>
 </template>
 
 <script>
     import Search from './components/Search.vue'
+    import MovieCarousel from './components/MovieCarousel'
 
 
     export default {
         name: 'App',
         components: {
-            Search
+            Search,
+            MovieCarousel
+        },
+        data: () => {
+            return {
+                posterData: Array,
+            }
         },
         props: {
             searchText: String
         },
-
+        methods: {
+            getPosters: function (posters) {
+                this.posterData = posters;
+            }
+        }
     }
 </script>
 
