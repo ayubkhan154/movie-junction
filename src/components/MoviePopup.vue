219416700528c4e7a02e9982bca09599eaf8dec7
Ayub Khan <ayubkhan154@live.com>
2020-04-10 10:42:11 -0400

    Added new component MoviePopup.vue

---
+++ src/components/MoviePopup.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="modal-mask">
+        <div class="modal-wrapper">
+            <div class="modal-container">
+
+                <div class="modal-header">
+                    <slot name="header">
+                        default header
+                    </slot>
+                </div>
+
+                <div class="modal-body">
+                    <slot name="body">
+                        default body
+                    </slot>
+                </div>
+
+                <div class="modal-footer">
+                    <slot name="footer">
+                        default footer
+                        <button @click="$emit('close')" class="modal-default-button">
+                            OK
+                        </button>
+                    </slot>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "MoviePopup"
+    }
+</script>
+
+<style scoped>
+
+</style>
