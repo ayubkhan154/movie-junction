23ae2e6d1784a57e52700be6401e32dd88448dde
Akshit Tripathi <tripathisoft@gmail.com>
2020-04-10 19:21:30 -0400

    MovieCarousel: Implemented bindings for the Modal state

--- src/components/MovieCarousel.vue
+++ src/components/MovieCarousel.vue
@@ -6,14 +6,17 @@
                            : 'https://popcornusa.s3.amazonaws.com/placeholder-movieimage.png'"
                        :title="poster.Title"
                        :year="poster.Year"
-                       v-for="poster in posters"/>
-        <MoviePopup v-if="showModal"/>
+                       @click="showPopup(poster.imdbID)"
+                       v-for="poster in posters"
+        />
+        <MoviePopup :item="selectedItem" @close="closeModal" v-if="showModal"/>
     </main>
 </template>
 
 <script>
     import MoviePortrait from "@/components/MoviePortrait";
     import MoviePopup from "@/components/MoviePopup";
+    import axios from "axios";
 
     export default {
         name: "MovieCarousel",
@@ -27,12 +30,20 @@
         data: () => {
             return {
                 showModal: false,
-                selectedItem: {}
+                selectedItem: {},
+                apiKey: 'e335d66d'
             }
         },
         methods: {
-            showPopup: () => {
-
+            showPopup: function (id) {
+                axios.get(`http://www.omdbapi.com/?apikey=${this.apiKey}&i=${id}&type=Movie`)
+                    .then(function (response) {
+                        this.selectedItem = response.data
+                    }.bind(this))
+                    .then(() => this.showModal = true);
+            },
+            closeModal: function () {
+                this.showModal = false;
             }
         }
     }
@@ -49,5 +60,19 @@
     main div {
         width: 100%;
         height: 100%;
+    }
+
+    .modal-enter {
+        opacity: 0;
+    }
+
+    .modal-leave-active {
+        opacity: 0;
+    }
+
+    .modal-enter .modal-container,
+    .modal-leave-active .modal-container {
+        -webkit-transform: scale(1.1);
+        transform: scale(1.1);
     }
 </style>
