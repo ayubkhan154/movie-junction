7a17668401c9da5fb012907f2af078aba1f1680c
Akshit Tripathi <tripathisoft@gmail.com>
2020-04-11 20:08:52 -0400

    Import FontAwesome icons

--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
 import Vue from "vue";
 import App from "./App.vue";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faClock, faCalendar } from "@fortawesome/pro-duotone-svg-icons";
+import { faImdb } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(faImdb);
+library.add(faClock);
+library.add(faCalendar);
+
+Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 
