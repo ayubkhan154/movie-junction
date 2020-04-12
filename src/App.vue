b3152436de9b08d8ab6f040312ff22c4ba9806c6
Akshit Tripathi <tripathisoft@gmail.com>
2020-04-11 20:06:17 -0400

    DARK MODE

--- src/App.vue
+++ src/App.vue
@@ -41,13 +41,22 @@
 </script>
 
 <style>
+    body {
+        margin: 0;
+        display: grid;
+        height: 100vh;
+    }
+
     #app {
         font-family: Avenir, Helvetica, Arial, sans-serif;
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
         text-align: center;
+
+        background-color: #0f0f0f;
         color: #2c3e50;
-        margin-top: 60px;
+        margin: 0;
+        padding: 0;
     }
 
     #spinner {
